4030570e36e879f6b163509cfdf40f76d10d56f2
JeyasuriyaaJeyakumar <jsuriya816@gmail.com>
2022-12-06 14:43:58 -0500

    no message

---
+++ Restaurant Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant Management
+//
+//  Created by Jeyasuriyaa Jeyakumar on 22/07/20.
+//  Copyright © 2020 Jeyasuriyaa Jeyakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
